1f22c2e07fc179473d2b62fa2a0d0222a09acc7f
IlyaOsotov <soten58@yandex.ru>
2019-03-15 22:03:10 +0300

    removed unused tests

--- spec/multiple_api_checker_spec.rb
+++ spec/multiple_api_checker_spec.rb
@@ -1,13 +1,5 @@
 # rubocop:disable Metrics/BlockLength
 RSpec.describe Client do
-  it 'has a version number' do
-    expect(MultipleApiChecker::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(true).to eq(true)
-  end
-
   subject { described_class }
 
   context 'when bank api does not registered' do
